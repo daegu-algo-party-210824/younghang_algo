daa6c8dec1456ffd8caa6813f7546c26d7008ceb
goddl717 <37294326+goddl717@users.noreply.github.com>
2021-08-27 20:32:46 +0900

    fix : 01.py

--- 기출/01.py
+++ 기출/01.py
@@ -5,13 +5,16 @@ arr = list(map(int,input().split()))
 ret = 0
 arr.sort()
 
-temp = arr[0]
+temp = 0 
 
 for i in range(len(arr)):
-    temp -= 1
-    if temp == 0 and (i+1) < len(arr):
-        temp = arr[i+1]
+    temp += 1
+    if temp >= arr[i] :
         ret +=1
+        temp = 0
+
+print(ret)
+
    
 
 
