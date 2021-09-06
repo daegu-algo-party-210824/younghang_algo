21154e8f621675bd69fb22e1dfd84ae0ef7d8a9a
goddl717 <37294326+goddl717@users.noreply.github.com>
2021-09-06 19:34:45 +0900

    Create 32.정수삼각형.py

---
+++ 기출/32.정수삼각형.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+arr = []
+dp = [[0]*(n) for i in range(n)]
+
+for i in range(n) :
+    arr.append(list(map(int,input().split())))
+
+dp[0][0] = arr[0][0]
+
+for i in range(n-1) :
+    for j in range(i+1):
+        dp[i+1][j] = max (dp[i+1][j], dp[i][j] + arr[i+1][j])
+        dp[i+1][j+1] = max (dp[i+1][j+1], dp[i][j] + arr[i+1][j+1])
+
+print(max (dp[n-1]))
+              
+
