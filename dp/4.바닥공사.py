5ce3ab86787d2f66db71cea514e7044b5663b45d
goddl717 <37294326+goddl717@users.noreply.github.com>
2021-09-07 16:45:11 +0900

    add : dp solution
    add : dp solution

---
+++ dp/4.바닥공사.py
@@ -0,0 +1,10 @@
+# 기본 문제 
+
+n = int(input())
+
+dp = [0] * (n+1)
+dp[0]=1
+dp[1]=1
+for i in range(1,n+1):
+    dp[i] = 2*dp[i-2] + dp[i-1]
+print(dp)
--- 기출/34.병사배치하기.py
+++ 기출/34.병사배치하기.py
@@ -1,15 +1,20 @@
+# 병사 배치하기
+# 초기에 dp를 모두 1로 갱신하는게 생각하기 어렵다
+# 시간복잡도는 n^2 이다
+# 전형적인 LIS 문제
 n = int(input())
 
 arr = list (map(int,input().split()))
 
-dp = [0] * n
-dp[0] =1
+dp = [1] * n
+
 
 for i in range(n) :
     for j in range(i) :
         if (dp[j]+1 >= dp[i] and arr[i] < arr[j]):
             dp[i]= dp[j]+1
 
-print(n - dp[n-1])
+print(n - max(dp))
+
 
 
