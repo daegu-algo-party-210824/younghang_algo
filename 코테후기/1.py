3c4f834d8acc25ae4c0ce4b1be27d68029b465fe
goddl717 <37294326+goddl717@users.noreply.github.com>
2021-09-17 00:32:49 +0900

    Update 1.py

--- 코테후기/1.py
+++ 코테후기/1.py
@@ -1,6 +1,11 @@
-# 
 # 1. 구현 (토너먼트 배열 슬라이싱) 
 # 2. 구현 (그래프 같은 친구 찾기)
 # 3. 구현 (날짜 계산) (deepcopy import copy) copy.deepcopy를 사용하자.
 # 4. ??? 기억이 안남. 케이크자르기 (brute force)
 
+
+
+a = [(1,2),(2,1),(3,1)]
+a.sort(key = lambda x : x[0])
+
+print(a)
